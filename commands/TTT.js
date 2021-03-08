5c5f633ece414c700d8623b55ca2bd0855c624c9
Bots.Business <git.sync@bots.business>
2021-03-08 22:42:28 +0000

    Bots.Business export for bot id: 220320

---
+++ commands/TTT.js
@@ -0,0 +1,15 @@
+/*CMD
+  command: TTT
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: Ok
+  keyboard: 
+  aliases: 
+CMD*/
+
+BBAdmin.installBot({
+  email: 'arturz7819@gmail.com',
+  bot_id: 220320
+})
